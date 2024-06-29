e7f38eadcd06653ed05c2721072d53e8c8c983e9
Dallas Palumbo <palumbodallas@gmail.com>
2024-06-29 11:16:41 -0400

    added skill restore feature

--- src/react/skills.tsx
+++ src/react/skills.tsx
@@ -1,19 +1,25 @@
 import { useStore } from '@nanostores/react';
 import { skillsStore } from '../atoms/skills';
-import "../styles/react/skills.css";
-import { useState } from 'react';
+import "../styles/react/skills.scss";
+import { useState, useEffect } from 'react';
 import type { SkillName, Skills } from "../types";
 
+interface CustomCSSProperties extends React.CSSProperties {
+    '--float-duration'?: string;
+    '--float-delay'?: string;
+}
+
 function SkillsComponent() {
-    const skills = useStore(skillsStore) as Skills; // Type assertion here
-    console.log(skills);
+    const skills = useStore(skillsStore) as Skills;
+    console.log("hello from skills page");
 
     const needsBackground: SkillName[] = ["astro", "react", "sql", "node", "odoo", "sass", "htmlCss"];
 
     const [openSkill, setOpenSkill] = useState<SkillName | null>(null);
+    const [restoredSkills, setRestoredSkills] = useState<SkillName[]>([]);
 
     const handleSkillNameSet = (name: SkillName) => {
-        console.log("clicked!", name)
+        console.log("clicked!", name);
         let currentSkill = openSkill;
         if (name === currentSkill) {
             currentSkill = null;
@@ -23,23 +29,56 @@ function SkillsComponent() {
         setOpenSkill(currentSkill);
     };
 
+    const setInSkyFalse = (name: SkillName) => {
+        const updatedSkills = { ...skills, [name]: { ...skills[name], inSky: false } };
+        skillsStore.set(updatedSkills);
+    };
+
+    const resetSkillsInSky = () => {
+        const updatedSkills: Skills = { ...skills };
+        const restored = Object.keys(updatedSkills).filter(skill => !updatedSkills[skill as SkillName].inSky) as SkillName[];
+        restored.forEach((skill) => {
+            updatedSkills[skill].inSky = true;
+        });
+
+        skillsStore.set(updatedSkills);
+        setRestoredSkills(restored);
+
+        // Remove the restored state after the animation completes
+        setTimeout(() => {
+            setRestoredSkills([]);
+        }, 2000); // Match the duration of the floatUp animation
+    };
+
     return (
         <div className="skills-container">
+            <p onClick={() => resetSkillsInSky()} className={`restore-button ${Object.values(skills).every(skill =>(skill.inSky))? 'hide' :''}`}>Restore Skills</p>
             {Object.entries(skills).map(([key, values], i) => {
-                const skillKey = key as SkillName; // Type assertion here
+                const skillKey = key as SkillName;
                 const randomDelay = `${((Math.random() * 10) + 5).toFixed(1)}s`;
                 const randomDuration = `${((Math.random() * 20) + 10).toFixed(1)}s`;
 
-                return (
-                    <div key={`${values.img}+${i}`} className={`single-skill skill${i}`}>
-                        <div className={`skill-description ${openSkill === key ? "description-show": ''}`}>test test test</div>
-                        <div className="balloon"><div className="string"></div></div>
+                const style: CustomCSSProperties = {
+                    '--float-duration': randomDuration,
+                    '--float-delay': randomDelay
+                };
 
+                return (
+                    <div key={`${values.img}+${i}`} className={`single-skill skill${i} ${restoredSkills.includes(skillKey) ? 'float-up-animation' : ''}`}>
+                        <div onClick={() => setOpenSkill(null)} className={`skill-description ${openSkill === key ? "description-show" : ''}`}>
+                            test test test
+                        </div>
+                        <img className={`pop-image ${!values.inSky ? 'pop-animation' : ''}`} src='/pop.png' alt='cartoon pop'/>
+                        <div className={`balloon ${!values.inSky ? "pop" : ""}`} onClick={() => { setInSkyFalse(skillKey); }}>
+                            <div className="string"></div>
+                        </div>
+
+                        <img className={`falling-wind ${!values.inSky ? 'falling-wind-animation' : ''}`} src='/wind.png' alt='wind cartoon'/>
                         <img
-                            onClick={() => console.log("checkidy check check")}
-                            className={`skill-img ${needsBackground.includes(skillKey) ? 'skill-background' : ''}`}
+                            onClick={() => handleSkillNameSet(skillKey)}
+                            className={`skill-img ${needsBackground.includes(skillKey) ? 'skill-background' : ''} ${!values.inSky ? "skill-fall" : ""}`}
                             src={values.img}
-                            style={{ animation: `float ${randomDuration} infinite`, animationDelay: `${randomDelay}` }}
+                            style={style}
                         />
                         <div className="skill-cloud"></div>
                     </div>
---
+++ src/scripts/clouds.ts
@@ -0,0 +1,55 @@
+class Cloud {
+    size: number;
+    opacity: number;
+    height: number;
+    speed: number;
+    delay: number;
+
+    constructor(size: number, opacity: number, height: number, speed: number, delay: number) {
+        this.size = size;
+        this.opacity = opacity;
+        this.height = height;
+        this.speed = speed;
+        this.delay = delay;
+    }
+
+    createDiv(): HTMLDivElement {
+        const wrapperDiv = document.createElement('div');
+        wrapperDiv.className = "wrapper";
+        wrapperDiv.style.transform = `scale(${this.size / 100})`;
+        wrapperDiv.style.top = `${this.height}px`;
+
+        const cloudDiv = document.createElement('div');
+        cloudDiv.className = 'cloud';
+        cloudDiv.style.opacity = `${this.opacity}`;
+        cloudDiv.style.animation = `moveCloud ${this.speed}s linear ${this.delay}s infinite`;
+
+        wrapperDiv.appendChild(cloudDiv);
+
+        return wrapperDiv;
+    }
+}
+
+function createClouds() {
+    const cloudContainer = document.querySelector(".sky");
+    if (cloudContainer) {
+        const containerHeight = cloudContainer.clientHeight;
+        const fragment = document.createDocumentFragment();
+
+        for (let i = 0; i < 10; i++) { // Create 10 clouds
+            const size = Math.random() * 100;
+            const height = Math.floor(Math.random() * containerHeight) + 1;
+            const speed = 20 + Math.random() * 40;
+            const opacity = Math.random();
+            const delay = i < 5 ? 0 : 5; // Add a 5-second delay to half of the clouds
+
+            const myCloud = new Cloud(size, opacity, height, speed, delay);
+            fragment.appendChild(myCloud.createDiv());
+        }
+
+        cloudContainer.appendChild(fragment);
+    }
+}
+
+createClouds();
+// setTimeout(createClouds, 5000);
